eb24f07a0ddc84d33cf90ef68d72dc6d17cfbfff
TheHackdes <q0jcgzzjk@mozmail.com>
2025-05-02 13:38:17 +0200

    Delete Nvim Local

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "ycc", function()
+  return "yy" .. vim.v.count1 .. "gcc']p"
+end, { remap = true, expr = true })
